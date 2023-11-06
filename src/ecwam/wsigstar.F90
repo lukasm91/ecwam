40a7aa273498ec39f8fb9396c37157d39db1937b
Lukas Mosimann <lmosimann@nvidia.com>
2023-11-06 05:58:59 -0800

    make wsigstar pointwise

--- src/ecwam/wsigstar.F90
+++ src/ecwam/wsigstar.F90
@@ -7,7 +7,8 @@
 ! nor does it submit to any jurisdiction.
 !
 
-      SUBROUTINE WSIGSTAR (KIJS, KIJL, WSWAVE, UFRIC, Z0M, WSTAR, SIG_N)
+      SUBROUTINE WSIGSTAR (WSWAVE, UFRIC, Z0M, WSTAR, SIG_N)
+!$loki routine seq
 ! ----------------------------------------------------------------------
 
 !**** *WSIGSTAR* - COMPUTATION OF THE RELATIVE STANDARD DEVIATION OF USTAR.
@@ -59,9 +60,8 @@
 
       IMPLICIT NONE
 
-      INTEGER(KIND=JWIM), INTENT(IN) :: KIJS, KIJL
-      REAL(KIND=JWRB), DIMENSION(KIJL), INTENT(IN) :: WSWAVE, UFRIC, Z0M, WSTAR
-      REAL(KIND=JWRB), DIMENSION(KIJL), INTENT(OUT) :: SIG_N
+      REAL(KIND=JWRB), INTENT(IN) :: WSWAVE, UFRIC, Z0M, WSTAR
+      REAL(KIND=JWRB), INTENT(OUT) :: SIG_N
 
       INTEGER(KIND=JWIM) :: IJ
 
@@ -83,22 +83,21 @@
 
 ! ----------------------------------------------------------------------
 
-      DO IJ=KIJS,KIJL
         IF (LLGCBZ0) THEN
           ZN = RNUM
           
-          U10M1 = 1.0_JWRB / MAX(WSWAVE(IJ), WSPMIN)
+          U10M1 = 1.0_JWRB / MAX(WSWAVE, WSPMIN)
           ! CHARNOCK:
-          Z0VIS = ZN / MAX(UFRIC(IJ), EPSUS)
-          ZCHAR = G*(Z0M(IJ) - Z0VIS) / MAX(UFRIC(IJ)**2, EPSUS)
+          Z0VIS = ZN / MAX(UFRIC, EPSUS)
+          ZCHAR = G*(Z0M - Z0VIS) / MAX(UFRIC**2, EPSUS)
           ZCHAR = MAX(MIN(ZCHAR, ALPHAMAX), ALPHAMIN)
           
           BCD = BCDLIN*SQRT(ZCHAR)
-          C_D = ACDLIN + BCD*WSWAVE(IJ)
+          C_D = ACDLIN + BCD*WSWAVE
           DC_DDU = BCD
-          SIG_CONV = 1.0_JWRB + 0.5_JWRB*WSWAVE(IJ) / C_D*DC_DDU
-          SIG_N(IJ) = MIN(SIG_NMAX, SIG_CONV*U10M1*(BG_GUST*UFRIC(IJ)**3 &
-               + 0.5_JWRB*XKAPPA*WSTAR(IJ)**3)**ONETHIRD)
+          SIG_CONV = 1.0_JWRB + 0.5_JWRB*WSWAVE / C_D*DC_DDU
+          SIG_N = MIN(SIG_NMAX, SIG_CONV*U10M1*(BG_GUST*UFRIC**3 &
+               + 0.5_JWRB*XKAPPA*WSTAR**3)**ONETHIRD)
           
         ELSE
           ZN = 0.0_JWRB
@@ -111,7 +110,7 @@
           !       BASED ON U*
           !
           XKAPPAD = 1.0_JWRB / XKAPPA
-          U10 = UFRIC(IJ)*XKAPPAD*(LOG10 - LOG(Z0M(IJ)))
+          U10 = UFRIC*XKAPPAD*(LOG10 - LOG(Z0M))
           U10 = MAX(U10, WSPMIN)
           U10M1 = 1.0_JWRB / U10
           C2U10P1 = C2*U10**P1
@@ -119,10 +118,9 @@
           C_D = (C1 + C2U10P1)*U10P2
           DC_DDU = (P2*C1 + (P1 + P2)*C2U10P1)*U10P2*U10M1
           SIG_CONV = 1.0_JWRB + 0.5_JWRB*U10 / C_D*DC_DDU
-          SIG_N(IJ) = MIN(SIG_NMAX, SIG_CONV*U10M1*(BG_GUST*UFRIC(IJ)**3 &
-              + 0.5_JWRB*XKAPPA*WSTAR(IJ)**3)**ONETHIRD)
+          SIG_N = MIN(SIG_NMAX, SIG_CONV*U10M1*(BG_GUST*UFRIC**3 &
+              + 0.5_JWRB*XKAPPA*WSTAR**3)**ONETHIRD)
           
         END IF
-      END DO
 
       END SUBROUTINE WSIGSTAR
